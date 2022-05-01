a36998c97ef29359d56a6dc5e2779cc5ec5a0c01
Juunini (쥬니니) <juuni.ni.i@gmail.com>
2022-05-01 15:25:16 +0900

    Set custom title and favicon by `window.XRCLOUD`

--- src/hub.js
+++ src/hub.js
@@ -1387,4 +1387,12 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   authChannel.setSocket(socket);
   linkChannel.setSocket(socket);
+
+  if (window.XRCLOUD) {
+    const { hubName, appName, faviconUrl } = window.XRCLOUD;
+
+    document.title = `${hubName} | ${appName}`;
+    document.querySelector('link[rel="icon"]').href = faviconUrl;
+    document.querySelector('link[rel="apple-touch-icon"]').href = faviconUrl;
+  }
 });
