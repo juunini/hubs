6a0f986e8ca51f2909503308eb5943a48bc378fd
jewoo <jeus0630@gmail.com>
2022-07-28 15:49:33 +0900

    Add function for avatar with head

--- src/components/player-info.js
+++ src/components/player-info.js
@@ -3,7 +3,7 @@ import { AVATAR_TYPES } from "../utils/avatar-utils";
 import { registerComponentInstance, deregisterComponentInstance } from "../utils/component-utils";
 import defaultAvatar from "../assets/models/DefaultAvatar.glb";
 import { MediaDevicesEvents } from "../utils/media-devices-utils";
-import { createHeadlessModelForSkinnedMesh } from "../utils/three-utils";
+import { createModelForSkinnedMesh } from "../utils/three-utils";
 import { Layers } from "./layers";
 
 function ensureAvatarNodes(json) {
@@ -77,10 +77,10 @@ AFRAME.registerComponent("player-info", {
       let isSkinnedAvatar = false;
       modelEl.object3D.traverse(function(o) {
         if (o.isSkinnedMesh) {
-          const headlessMesh = createHeadlessModelForSkinnedMesh(o);
-          if (headlessMesh) {
+          const mesh = createModelForSkinnedMesh(o);
+          if (mesh) {
             isSkinnedAvatar = true;
-            o.parent.add(headlessMesh);
+            o.parent.add(mesh);
           }
         }
       });
